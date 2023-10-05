ef4d4d70165c317f389ec1576b209926d336f6a6
Ujjwal Jha <ujjwaljha1225@gmail.com>
2023-10-06 03:45:30 +0545

    Initial setup of our contact application manager

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.get("/api/contacts", (req, res) => {
+  res.json({ message: "Get all contacts" });
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
