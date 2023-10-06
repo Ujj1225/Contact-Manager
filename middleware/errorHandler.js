ad587ff01c06423e0740d583e7b6f1ed447a32f6
Ujjwal Jha <ujjwaljha1225@gmail.com>
2023-10-06 05:48:36 +0545

    Middleware to handle errors

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,8 +1,24 @@
-const errorHandler =  (err, req, res, next) =>{
-    // This below syntax means if there is a statusCode give it res.statusCode
-    // Else provide with the value 500
-    const statusCode = res.statusCode ? res.statusCode: 500;
-    res.json({message: err.message, stackTrace: err.stack});
-    
+const errorHandler = (err, req, res, next) => {
+  // This below syntax means if there is a statusCode give it res.statusCode
+  // Else provide with the value 500
+  const statusCode = res.statusCode ? res.statusCode : 500;
+  switch (statusCode) {
+    case 400:
+      res.json({
+        title: "Validation Error",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+    case 404:
+      res.json({
+        title: "Not found!",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+    default:
+        break;
+  }
 };
 module.exports = errorHandler;
